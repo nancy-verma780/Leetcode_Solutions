d10d9b4fbb94eb680c79da0ffca7975985a5d5c2
Nancy Verma <nancy45815@gmail.com>
2026-07-18 21:21:50 +0530

    Time: 23 ms (49.04%), Space: 16.1 MB (68.09%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        from collections import defaultdict
+
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = "".join(sorted(word))
+            groups[key].append(word)
+
+        return list(groups.values())
